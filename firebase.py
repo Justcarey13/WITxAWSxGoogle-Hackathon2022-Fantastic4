272e5744f4b6cf436ccff252d7002212b64e4394
Carey Yang <just.carey@gmail.com>
2022-09-10 22:15:45 +1000

    feat: create dummy pipe data

--- firebase.py
+++ firebase.py
@@ -1,6 +1,14 @@
 
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate("fantastic-4-17e97-firebase-adminsdk-bt1mc-455424f7ee.json")
 firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+# Create dummy data
+pipe_dummy_data = {'id': '1', ' material ': 'steel', 'age': 10, 'diameter': 300, 'pressure': 1.1, 'depth': 0.7 }
+db.collection('infrastructure').document('10-09-2022').collection('pipe').add(pipe_dummy_data)
+
